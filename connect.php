62295284da99b336f15cc2a6a28301d67bbb62ba
Walter Sanchez <walteranyika@gmail.com>
2018-05-26 10:23:25 +0300

    Efficient Backend V2

--- connect.php
+++ connect.php
@@ -1,3 +1,3 @@
 <?php
 //$conn=mysqli_connect("localhost","root","","taxi");
-$conn=mysqli_connect("localhost","root","","combined");
+$conn=mysqli_connect("localhost","root","Normal@2018","combined");
